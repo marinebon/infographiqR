63d06793ee2f9e7639c7006af0c28ecc8ebd7c15
Ben Best <ben@ecoquants.com>
2019-07-02 13:02:50 -0700

    + explicit jquery & bootstrap (for blogdown) JS dependencies, + debug arg to info_svg() fixes marinebon/fk-iea#4

--- inst/infographiq.js
+++ inst/infographiq.js
@@ -19,13 +19,16 @@ d3.svg(options.svg).then((f) => {
   svg.select("#questions")
     .attr("display", "none");
 
-    
-  console.log('test before data.forEach');
+  if (options.debug){ 
+    console.log('test before data.forEach');
+  }
   
   // assign links
   data.forEach(function(d) {
-    console.log('test in data.forEach');
-    console.log('forEach d.id: ' + d.id);
+    if (options.debug){ 
+      console.log('test in data.forEach');
+      console.log('forEach d.id: ' + d.id);
+    }
     
     // reset fill in group id and children
     h.selectAll('#' + d.id)
@@ -45,7 +48,10 @@ d3.svg(options.svg).then((f) => {
         if (d.link_nonmodal > ''){
           window.location = d.link_nonmodal;
         } else {
-          console.log('  link_modal:' + d.id);
+          
+          if (options.debug){ 
+            console.log('  link_modal:' + d.id);
+          }
           
           $('#'+ options.modal_id).find('iframe')
             .prop('src', function(){ return d.link_modal });
@@ -63,7 +69,9 @@ d3.svg(options.svg).then((f) => {
      //.on('mouseover', handleMouseOver)
      //.on('mouseout', handleMouseOut);
      .on('mouseover', function() {
-       console.log('  mouseover():' + d.id);
+       if (options.debug){ 
+        console.log('  mouseover():' + d.id);
+       }
        
        d3.select(this)
         .style("fill", options.color_hover)
@@ -79,7 +87,9 @@ d3.svg(options.svg).then((f) => {
        //highlight();
      })
      .on('mouseout', function() {
-       console.log('  mouseout():' + d.id);
+       if (options.debug){ 
+        console.log('  mouseout():' + d.id);
+       }
        
        d3.select(this)
         .style("fill", options.color_default)
